6c97cffddd104334c9585c2f50b46f5da5d9f052
Benjamin <bvanbever1@gmail.com>
2019-01-11 21:19:17 +0100

    added message class. Runned migration

--- mysite/models.py
+++ mysite/models.py
@@ -9,3 +9,8 @@ class Person(models.Model):
     home_phone_number = models.CharField(max_length = 20)
     cellphone_number = models.CharField(max_length = 20)
     password = models.CharField(max_length = 32)
+
+class Message(models.Model):
+    author = models.ForeignKey('Person', on_delete=models.CASCADE)
+    content = models.TextField()
+    publication_date = models.DateField()
